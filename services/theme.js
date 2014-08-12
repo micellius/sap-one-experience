8273c18493d7e16e6dfdb6cac7b8a6e46d7894d8
Vadim Tomnikov <vadim.tomnikov@sap.com>
2014-08-12 23:54:44 +0300

    Generate theme list from file system. Closes #21.

--- services/theme.js
+++ services/theme.js
@@ -1,26 +1,20 @@
 /**
  * Created by i070970 on 8/12/14.
  */
+var fs = require('fs'),
+    path = require('path'),
+    themes = listThemeDirectories();
+
+function listThemeDirectories() {
+    var pathToThemes = path.normalize('./public/stylesheets/themes');
+    return fs.readdirSync(pathToThemes).filter(function (file) {
+        return fs.statSync(path.join(pathToThemes, file)).isDirectory();
+    });
+}
+
 exports.getThemes = function(req, res) {
     res.json({
         status: 'OK',
-        results: [
-            'amelia',
-            'cerulean',
-            'cosmo',
-            'cyborg',
-            'darkly',
-            'default',
-            'flatly',
-            'journal',
-            'lumen',
-            'readable',
-            'simplex',
-            'slate',
-            'spacelab',
-            'superhero',
-            'united',
-            'yeti'
-        ]
+        results: themes
     });
 };
